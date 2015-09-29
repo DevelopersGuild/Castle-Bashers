c6fc7575453bc3d1c15768e8ced2004827deee5f
Nir Ovadia <Ovadia.nir1@gmail.com>
2015-09-29 10:12:57 -0700

    rnd areas working

---
+++ Assets/Scripts/Area.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Area : MonoBehaviour
+{
+
+     public string numID;
+     public List<Area> transitions = new List<Area>();
+
+     // Use this for initialization
+     void Start()
+     {
+
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+
+     }
+
+     public Area getTransition(Area ar)
+     {
+          string s = numID + ar.getID();
+          foreach (Area trans in transitions)
+          {
+               if (trans.getID().Equals(s))
+               {
+                    return trans;
+               }
+          }
+          return null;
+     }
+
+     public string getID()
+     {
+          return numID;
+     }
+}
