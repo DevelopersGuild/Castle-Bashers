6affb9d92e330e6f5dee4b19e2cfa21c55baad25
Johnny Nguyen <jonthnngyn@gmail.com>
2015-10-18 20:54:29 -0700

    Start tracking audio files, Get a basic coin manager, fix filter on player sprite sheet

--- Assets/Scripts/LevelObjects/Coin.cs
+++ Assets/Scripts/LevelObjects/Coin.cs
@@ -9,9 +9,15 @@ public class Coin : MonoBehaviour {
     // Use this for initialization
     public void OnTriggerEnter(Collider other)
     {
-        // Play sound and destory objecton pickup
+        // Incremount coin count, play sound, and destroy object on pickup
         if (other.gameObject.CompareTag("Player"))
         {
+            if(other.GetComponent<CoinManager>())
+            {
+                CoinManager coinManager = other.GetComponent<CoinManager>();
+                coinManager.addCoins(coinValue);
+            }
+
             if(pickupSound)
             {
                 AudioSource.PlayClipAtPoint(pickupSound, transform.position);
---
+++ Assets/Scripts/Mechanics/CoinManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinManager : MonoBehaviour {
+
+    private int coins;
+
+    //Coin methods
+    public void addCoins(int add)
+    {
+        coins += add;
+    }
+
+    public void subtractCoins(int subtract)
+    {
+        coins -= subtract;
+    }
+
+    public void reset()
+    {
+        coins = 0;
+    }
+
+    public void setCoins(int coinsSet)
+    {
+        coins = coinsSet;
+    }
+
+}
