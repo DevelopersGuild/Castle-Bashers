8a62c7c39c38c44635bf8103b9e753a9be3455c6
Nir Ovadia <Ovadia.nir1@gmail.com>
2015-10-22 00:19:14 -0700

    Created a working trap with proximity activation and destruction upon dealing damage or after a small amount of time

--- Assets/Scripts/zzNir's test scripts/DealDamageToPlayer.cs
+++ Assets/Scripts/zzNir's test scripts/DealDamageToPlayer.cs
@@ -4,84 +4,81 @@ using System.Collections;
 public class DealDamageToPlayer : MonoBehaviour
 {
 
-     public float dmgAmount = 1;
+    public float dmgAmount = 1;
 
-     // Use this for initialization
-     void Start()
-     {
+    // Use this for initialization
+    void Start()
+    {
 
-          //GetComponent Enemy or Hazard
-          //get damage amount from Enemy or Hazard
-          //Enemy can change his damage amount
-     }
+        //GetComponent Enemy or Hazard
+        //get damage amount from Enemy or Hazard
+        //Enemy can change his damage amount
+    }
 
-     // Update is called once per frame
-     void Update()
-     {
+    // Update is called once per frame
+    void Update()
+    {
 
-     }
+    }
 
-     //If we make colliders appear on attacks, create OnCollisionEnter and OnTriggerEnter collisions
-     //destroy collider after they hit something
+    //If we make colliders appear on attacks, create OnCollisionEnter and OnTriggerEnter collisions
+    //destroy collider after they hit something
 
-     public void OnCollisionStay(Collision other)
-     {
-          //Check for player collision
-          if (other.gameObject.GetComponent<Player>())
-          {
-               //Find components necessary to take damage and knockback
-               GameObject playerObject = other.gameObject;
-               Player player = playerObject.GetComponent<Player>();
-               PlayerHealth hp = playerObject.GetComponent<PlayerHealth>();
-               //Take damage if the player isnt already currently invincible
-               if (!player.getInvincible())
-               {
-                    //Deal damage, knockback
-                    //get amt (1), dmgAmount(1) from Enemy/Hazard
-                    hp.takeDamage(1);
-                    player.setInvTime(0.5f);
-               }
-
-            if (GetComponent<Projectile>())
+    public void OnCollisionStay(Collision other)
+    {
+        //Check for player collision
+        if (other.gameObject.GetComponent<Player>())
+        {
+            //Find components necessary to take damage and knockback
+            GameObject playerObject = other.gameObject;
+            Player player = playerObject.GetComponent<Player>();
+            PlayerHealth hp = playerObject.GetComponent<PlayerHealth>();
+            //Take damage if the player isnt already currently invincible
+            if (!player.getInvincible())
             {
-                if (tag == "DProj")
-                    Destroy(gameObject);
+                //Deal damage, knockback
+                //get amt (1), dmgAmount(1) from Enemy/Hazard
+                hp.takeDamage(1);
+                player.setInvTime(0.5f);
             }
+
+            if (tag == "DProj")
+                Destroy(gameObject);
+
         }
-         
-     }
 
-     //Same code just make sure it happens
-     public void OnTriggerStay(Collider other)
-     {
+    }
 
-          //Check for player collision
-          if (other.gameObject.GetComponent<Player>())
-          {
-               //Find components necessary to take damage and knockback
-               GameObject playerObject = other.gameObject;
-               Player player = playerObject.GetComponent<Player>();
-               PlayerHealth hp = playerObject.GetComponent<PlayerHealth>();
+    //Same code just make sure it happens
+    public void OnTriggerStay(Collider other)
+    {
 
-               //Take damage if the player isnt already currently invincible
-               if (!player.getInvincible())
-               {
-                    //Deal damage, knockback
-                    //get amt (1), dmgAmount(1) from Enemy/Hazard
-                    hp.takeDamage(1);
-                    player.setInvTime(0.5f);
-                    if(CompareTag("One Time"))
-                    {
-                         Destroy(gameObject);
-                    }
-               }
+        //Check for player collision
+        if (other.gameObject.GetComponent<Player>())
+        {
+            //Find components necessary to take damage and knockback
+            GameObject playerObject = other.gameObject;
+            Player player = playerObject.GetComponent<Player>();
+            PlayerHealth hp = playerObject.GetComponent<PlayerHealth>();
 
-            if (GetComponent<Projectile>())
+            //Take damage if the player isnt already currently invincible
+            if (!player.getInvincible())
             {
-                if (tag == "DProj")
+                //Deal damage, knockback
+                //get amt (1), dmgAmount(1) from Enemy/Hazard
+                hp.takeDamage(1);
+                player.setInvTime(0.5f);
+                if (CompareTag("One Time"))
+                {
                     Destroy(gameObject);
+                }
             }
+
+
+            if (tag == "DProj")
+                Destroy(gameObject);
+
         }
-     }
+    }
 }
 
