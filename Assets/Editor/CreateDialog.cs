605c3bff60ae3c413c231ba61141132a5190193c
kroulis <qinkroulis@gmail.com>
2016-01-22 13:33:52 -0800

    writing plugin of dialog system (10% finished)

---
+++ Assets/Editor/CreateDialog.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+//using Kroulis.Dialog;
+
+public class CreateDialog : EditorWindow
+{
+    [MenuItem("Tools/Manage Dialog")]
+    static void ManageDialog () 
+    {
+        Rect wr = new Rect(0, 0, 500, 500);
+        CreateDialog window = (CreateDialog)EditorWindow.GetWindowWithRect(typeof(CreateDialog), wr, true, "Dialog Manager");
+        window.Show();
+    }
+
+    Object NPC;
+    string dialog_id;
+    //NormalDialog dial = new NormalDialog();
+
+    void OnGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        NPC = EditorGUILayout.ObjectField("NPC GameObject", Selection.activeGameObject, typeof(GameObject));
+        dialog_id=EditorGUILayout.TextField("Dialog ID","");
+        EditorGUILayout.EndVertical();
+
+        if(GUILayout.Button("Check ID"))
+        {
+            //if(dial.CheckIDAvailable(dialog_id))
+            //{
+            //    //this.ShowNotification(new GUIContent("Dialog ID "+dialog_id+" is available."));
+                
+            //}
+            //else
+            //{
+            //    //this.ShowNotification(new GUIContent("Dialog ID " + dialog_id + " is not available."));
+            //}
+        }
+    }
+}
