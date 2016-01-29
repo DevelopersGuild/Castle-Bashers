a27771f153a2026231efd7b77d0260db9328bb8c
kroulis <qinkroulis@gmail.com>
2016-01-29 15:34:05 -0800

    Dialog System post-action example. (Town Ranger Shop....)

---
+++ Assets/Scripts/Kroulis Scripts/Example_Dialog_Action.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Kroulis.Dialog;
+
+namespace Kroulis.Dialog.Objects
+{
+    public class Example_Dialog_Action : DialogAction
+    {
+
+        public override void Action()
+        {
+            Main_Process main_process = GetComponent<NPCDialogControl>().GetMainProcess();
+            main_process.OpenDialog("10000", GetComponent<NPCDialogControl>().NPC_Name);
+        }
+    }
+}
+
