312ec5c48bfccabaafa6e6b1ad1fe8001b6fb11c
kroulis <qinkroulis@gmail.com>
2016-02-26 14:42:36 -0800

    fixed bug: teleport.

--- Assets/Scripts/Kroulis Scripts/PositionInit.cs
+++ Assets/Scripts/Kroulis Scripts/PositionInit.cs
@@ -6,7 +6,12 @@ public class PositionInit : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         GameObject playerholder = GameObject.Find("PlayerHolder");
-        playerholder.transform.position = transform.position;
+        //playerholder.transform.position = transform.position;
+        Player[] pls = playerholder.GetComponentsInChildren<Player>();
+        foreach(Player pl in pls)
+        {
+            pl.transform.position = new Vector3(12.4f, 0f, 0f);
+        }
 	}
 	
 }
