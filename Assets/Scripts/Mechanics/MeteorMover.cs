fb9d084b626a6c5e6ad088f84163cc6991e5bdd9
Johnny <jonthnngyn@gmail.com>
2016-03-11 00:51:16 -0800

    Delete duplicate sounds, fix broken sounds, add more particle fx, fix projectile, fix meteor

--- Assets/Scripts/Mechanics/MeteorMover.cs
+++ Assets/Scripts/Mechanics/MeteorMover.cs
@@ -5,9 +5,13 @@ public class MeteorMover : MonoBehaviour {
     private Vector3 moveIncrement;
     public float speed = 1;
     public float speedIncrement = 0; //speeds meteor up by this amount each frame
+    public GameObject destroyedParticle;
+    public AudioClip destroyedSound;
+    private DealDamage dealDamage;
 	// Use this for initialization
 	void Start () {
         moveIncrement = new Vector3(-1, -2, 0);
+	    dealDamage = GetComponent<DealDamage>();
         Destroy(gameObject, 5);
 	}
 	
@@ -17,10 +21,24 @@ public class MeteorMover : MonoBehaviour {
         gameObject.transform.position += moveIncrement * speed;
 	}
 
-    void OnTriggerEnter(Collider col)
+    void OnTriggerEnter(Collider other)
     {
-        if(col.gameObject.layer == LayerMask.NameToLayer("Platforms"))
+        if(other.gameObject.layer == LayerMask.NameToLayer("Platforms"))
         {
+            Destroy(gameObject);
+        }
+
+        if (((other.GetComponent<Player>() && dealDamage.damagesPlayers) || (other.GetComponent<Enemy>() && dealDamage.damagesEnemies)) || other.gameObject.layer == LayerMask.NameToLayer("Platforms"))
+        {
+            if (destroyedParticle)
+            {
+                GameObject particle = (GameObject)Instantiate(destroyedParticle, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), Quaternion.identity);
+            }
+
+            if (destroyedSound)
+            {
+                AudioSource.PlayClipAtPoint(destroyedSound, transform.position, 1);
+            }
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/zzNir's test scripts/Projectile.cs
+++ Assets/Scripts/zzNir's test scripts/Projectile.cs
@@ -3,31 +3,37 @@ using System.Collections;
 
 public class Projectile : SkillOld
 {
-     public float projectileSpeed;
-     public float damageAmount;
-     public float TimeToLive;
+    public float projectileSpeed;
     public bool destroyOnCollision;
+    public GameObject destroyedParticle;
+    public AudioClip destroyedSound;
+    public float TimeToLive;
+    private DealDamage dealDamage;
 
-     public void Start()
-     {
-          Destroy(transform.gameObject, TimeToLive);
-     }
+    public void Start()
+    {
+        dealDamage = GetComponent<DealDamage>();
+        Destroy(transform.gameObject, TimeToLive);
+    }
 
-     public void Update()
-     {
-          
-     }
-
-     //add rotation too
-     public void Shoot(Vector3 dir)
-     {
-          GetComponent<Rigidbody>().velocity = dir * projectileSpeed;
-     }
+    //add rotation too
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().velocity = dir * projectileSpeed;
+    }
 
     public void OnTriggerEnter(Collider other)
     {
-        if(destroyOnCollision)
+        if (((other.GetComponent<Player>() && dealDamage.damagesPlayers) || (other.GetComponent<Enemy>() && dealDamage.damagesEnemies)) && destroyOnCollision)
         {
+            if (destroyedParticle)
+            {
+                GameObject particle = (GameObject)Instantiate(destroyedParticle, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), Quaternion.identity);
+            }
+            if (destroyedSound)
+            {
+                AudioSource.PlayClipAtPoint(destroyedSound, transform.position, 1);
+            }
             Destroy(gameObject);
         }
     }
