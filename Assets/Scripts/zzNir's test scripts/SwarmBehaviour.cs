a39f8844f3583c6f5b1b86889f037d84405092b4
Johnny <jonthnngyn@gmail.com>
2016-03-16 17:15:45 -0700

    Create animations for swarm and hair

--- Assets/Scripts/zzNir's test scripts/SwarmBehaviour.cs
+++ Assets/Scripts/zzNir's test scripts/SwarmBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 public class SwarmBehaviour : MonoBehaviour {
 
@@ -9,10 +10,13 @@ public class SwarmBehaviour : MonoBehaviour {
     private Rigidbody rigBod;
     //private MoveController moveCon;
     private Malady mal;
+    private Animator animator;
     private Vector3 direction, currentPos, max;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+	    animator = GetComponent<Animator>();
         Duration -= 1;
         max = new Vector3(20, 5, 10);
         player = FindObjectOfType<Player>().gameObject;
@@ -32,6 +36,7 @@ public class SwarmBehaviour : MonoBehaviour {
         //anim set 3 = velocity > 1.1f;
         //anim set 4 = if velocity != direction && velocity < 0.7f;
         //maybe have no slow down in turns? we'll see
+        animator.SetFloat("Speed", rigBod.velocity.magnitude);
 
         if(flip)
         {
@@ -39,7 +44,7 @@ public class SwarmBehaviour : MonoBehaviour {
             Debug.Log(flip + " heyyyyy " + transform.localScale);
             flip = false;
         }
-        if (false)
+        if (true)
         {
             if (transform.position.y <= 2f)
             {
